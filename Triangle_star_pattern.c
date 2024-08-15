d2a00fcbfecd6f06b46c57aed67c83f301de46f9
PrajwalSinghBhadouria <151111886+PrajwalSinghBhadouria@users.noreply.github.com>
2024-08-15 09:01:31 +0530

    Add files via upload

---
+++ Triangle_star_pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	int n=4;
+	for(i=0; i<5; i++){
+		for(j=0; j<n; j++){
+			printf(" ");
+		}
+		n--;
+		for(j=0; j<i+1; j++){
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
